cd40d1ad939b5e527af4980994e7434a0e672395
Shiv981 <125433460+Shiv981@users.noreply.github.com>
2023-09-12 10:04:19 +0530

    Update client.cpp

--- client.cpp
+++ client.cpp
@@ -6,13 +6,41 @@
  */
 
 
-
+#define _WIN32_WINNT 0x0501
 #include <bits/stdc++.h>
 #include <winsock2.h>
-#define PORT 9909
+#include "mingw.thread.h"
+#include "mingw.invoke.h"
+
+#define PORT 9998
 using namespace std;
 struct sockaddr_in srv;
+int nsocket;
+void receive() {
+    char sbuff[1024] = {0};
+    int rev;
 
+    while (true) {
+        rev = recv(nsocket, sbuff, sizeof(sbuff), 0);
+        if (rev <= 0) {
+            cout << "Client disconnected or error occurred." << endl;
+            break;
+        }
+        sbuff[rev] = '\0';
+        cout << "msg from server : " << sbuff << endl;
+    }
+}
+
+void sendMsg() {
+    while (true) {
+        cout << "send msg to the server: ";
+        char input[1024];
+        cin.getline(input, sizeof(input));
+
+        // Send the message to the client multiple times (e.g., 5 times)
+            send(nsocket, input, strlen(input) + 1, 0);
+    }
+}
 int main(){
 	int nret=0;
 	    WSADATA ws;
@@ -21,7 +49,7 @@ int main(){
 	    } else {
 	        cout << "WSA created"<<endl;
 	    }
-	    int nsocket=socket(AF_INET,SOCK_STREAM,IPPROTO_TCP);
+	    nsocket=socket(AF_INET,SOCK_STREAM,IPPROTO_TCP);
 	    srv.sin_family=AF_INET;
 	       srv.sin_port=htons(PORT);
 	       srv.sin_addr.s_addr=inet_addr("127.0.0.1");
@@ -36,9 +64,17 @@ int main(){
 	      else{
 	    	  cout<<"connected to the server"<<endl;
 	      }
+	         thread t1(receive);
+	         thread t2(sendMsg);
 
+
+	              t1.join();
+	              t2.join();
+	        while(true){
+
+	        }
+	        closesocket(nsocket);
 }
-
 
 
 
